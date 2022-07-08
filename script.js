644ba335806dea45e1a7daa1ab4c2ffddf44cd6b
Maacieekk <nie1wiem2@gmail.com>
2022-07-08 21:32:28 +0200

    egzamin

---
+++ script.js
@@ -0,0 +1,11 @@
+
+ const dom = document.querySelector('.imieNazwisko')
+ dom.innerHTML = "Maciej Smyrak"
+
+ function clickMe(){
+
+    const clickme = document.querySelector('.click')
+    clickme.innerHTML = "Maciej Smyrak"   
+
+
+    }
